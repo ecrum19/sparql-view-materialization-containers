8f78e4682f96cf2b9798106ef59f4ef693398d09
ecrum19 <elicrum88@gmail.com>
2025-09-10 14:20:31 +0200

    build script changes

---
+++ local-biblio.js
@@ -0,0 +1,42 @@
+var localBibliography = {
+  "SPARQL11": {
+    title: "SPARQL 1.1 Query Language",
+    href: "https://www.w3.org/TR/sparql11-query/",
+    publisher: "W3C",
+  },
+  "SPARQLSD": {
+    title: "SPARQL 1.1 Service Description",
+    href: "https://www.w3.org/TR/sparql11-service-description/",
+    publisher: "W3C",
+  },
+  "LDP": {
+    authors: ["Steve Speicher", "John Arwe", "Ashok Malhotra"],
+    href: "https://www.w3.org/TR/ldp",
+    title: "Linked Data Platform 1.0",
+    rawDate: "2015-02-26",
+    status: "REC",
+    publisher: "W3C",
+  },
+  "SOLID-SPEC": {
+    authors: ["Sarven Capadisli", "Tim Berners-Lee", "Kjetil Kjernsmo"],
+    href: "https://solidproject.org/TR/protocol",
+    title: "Solid Protocol",
+    rawDate: "2024-05-12",
+    status: "CG-DRAFT",
+    publisher: "Solid Community Group",
+  },
+  "SHACL": {
+    authors: ["Holger Knublauch", "Dimitris Kontokostas"],
+    href: "https://www.w3.org/TR/shacl/",
+    title: "Shapes Constraint Language (SHACL)",
+    rawDate: "2017-07-20",
+    status: "REC",
+    publisher: "W3C",
+  },
+  "RDF11": {
+    title: "RDF 1.1 Concepts and Abstract Syntax",
+    href: "https://www.w3.org/TR/rdf11-concepts/",
+    publisher: "W3C",
+  },
+  
+};
